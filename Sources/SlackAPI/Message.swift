3c0d5e87be83addfa90678322378bae5b9de27db
Diogo Tridapalli <diogot@users.noreply.github.com>
2025-04-18 10:37:08 -0300

    Make user in Message be optional
    Signed-off-by: Diogo Tridapalli <diogot@users.noreply.github.com>

--- Sources/SlackAPI/Message.swift
+++ Sources/SlackAPI/Message.swift
@@ -9,7 +9,7 @@ import Foundation
 
 public struct Message: Decodable {
     public let ts: String
-    public let user: String
+    public let user: String?
     public let text: String
     public let type: String
 
@@ -23,7 +23,7 @@ public struct Message: Decodable {
     public init(from decoder: any Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         self.ts = try container.decode(String.self, forKey: .ts)
-        self.user = try container.decode(String.self, forKey: .user)
+        self.user = try container.decodeIfPresent(String.self, forKey: .user)
         self.text = try container.decode(String.self, forKey: .text)
         self.type = try container.decode(String.self, forKey: .type)
     }
