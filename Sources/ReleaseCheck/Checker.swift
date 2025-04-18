9ad17178b11f1854903a24c87de44b77f2a34f17
Diogo Tridapalli <diogot@users.noreply.github.com>
2025-04-18 10:36:56 -0300

    Delay SlackAPI requests
    Signed-off-by: Diogo Tridapalli <diogot@users.noreply.github.com>

--- Sources/ReleaseCheck/Checker.swift
+++ Sources/ReleaseCheck/Checker.swift
@@ -12,8 +12,9 @@ import TunesAPI
 class Checker {
     private let tunes: TunesAPI
     private let slack: SlackAPI
-    private let userId: String
-    private let channelId: String
+    private let channelName: String
+    private var _userId: String?
+    private var _channelId: String?
 
     var verbose: Bool {
         didSet {
@@ -26,9 +27,8 @@ class Checker {
         self.tunes = TunesAPI(verbose: verbose)
         let slack = SlackAPI(token:slackToken, verbose: verbose)
         self.slack = slack
-        self.userId = try await slack.whoAmI().userId
-        self.channelId = try await slack.channel(withName: channelName).id
         self.verbose = verbose
+        self.channelName = channelName
     }
 
     func publishRelease(of appId: String, on platform: Platform) async throws {
@@ -36,8 +36,8 @@ class Checker {
         let release = try await tunes.fetchCurrentRelease(appId: appId, platform: platform)
         let existentMessage = try await slack.searchMessage(
             with: release.messageId,
-            inChannel: channelId,
-            fromUser: userId,
+            inChannel: channelId(),
+            fromUser: userId(),
             after: release.date
         )
 
@@ -46,15 +46,15 @@ class Checker {
             return
         }
 
-        try await slack.postMessage(toChannelWithId: channelId, release.slackMessage)
+        try await slack.postMessage(toChannelWithId: channelId(), release.slackMessage)
         print("Release published for \(appId) on \(platform)!")
     }
 
     func deleteAllMessages() async throws {
         print("Deleting all messages...")
-        let messages = try await slack.searchMessages(with: nil, inChannel: channelId, fromUser: userId)
+        let messages = try await slack.searchMessages(with: nil, inChannel: channelId(), fromUser: userId())
         for message in messages {
-            try await slack.deleteMessage(message.ts, inChannel: channelId)
+            try await slack.deleteMessage(message.ts, inChannel: channelId())
         }
         print("All messages deleted!")
     }
@@ -62,5 +62,25 @@ class Checker {
     func printRelease(of appId: String, on platform: Platform) async throws {
         let release = try await tunes.fetchCurrentRelease(appId: appId, platform: platform)
         print(release.slackMessage)
+    }
+
+    private func userId() async throws -> String {
+        if let userId = _userId {
+            return userId
+        }
+        
+        let userInfo = try await slack.whoAmI()
+        self._userId = userInfo.user
+        return userInfo.user
+    }
+
+    private func channelId() async throws -> String {
+        if let channelId = _channelId {
+            return channelId
+        }
+
+        let channelId = try await slack.channel(withName: channelName).id
+        self._channelId = channelId
+        return channelId
     }
 }
